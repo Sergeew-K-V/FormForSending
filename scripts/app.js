d1c28c3549e009cc9613733b499a34c7b1aaeb41
Kirill <sergeew_k@mail.ru>
2022-02-02 19:06:57 +0300

    next select custom and file

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=PqTrhfjLQBI
+// https://novocherkassk.hh.ru/search/vacancy?schedule=remote&clusters=true&ored_clusters=true&experience=noExperience&enable_snippets=true&salary=&text=React+developer+junior
+// https://novocherkassk.hh.ru/vacancy/51644823?from=vacancy_search_list&hhtmFrom=vacancy_search_list&query=%D0%A1%D1%82%D0%B0%D0%B6%D1%91%D1%80-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20React
+// https://novocherkassk.hh.ru/vacancy/51264185?from=vacancy_search_list&hhtmFrom=vacancy_search_list&query=%D0%A1%D1%82%D0%B0%D0%B6%D1%91%D1%80-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20React
+// https://novocherkassk.hh.ru/vacancy/50656441?from=vacancy_search_list&hhtmFrom=vacancy_search_list&query=%D0%A1%D1%82%D0%B0%D0%B6%D1%91%D1%80-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%20React
+// https://novocherkassk.hh.ru/search/vacancy?clusters=true&ored_clusters=true&experience=noExperience&enable_snippets=true&salary=&text=%D0%A1%D1%82%D0%B0%D0%B6%D1%91%D1%80-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA+React
+
+//    Здравствуйте, я начинающий frontend developer, и хотел бы попасть на стажировку по вакансии "Стажер- Frontend разработчик". Очень хочу набраться опыта и улучшить свои знания, работая за реальными проектами. В 2021 году получил красный диплом бакалавра по специальности "Программная инженерия". Также сам учился и проходил курсы в интернете по frontend, делал проекты и выкладывал результаты на gitHub. Ссылка на gitHub в резюме.
+// Вот ссылка на мое резюме:https://drive.google.com/file/d/1tV-jo-2W6nw5ea2DFEE5YLdg37tWnWKv/view
